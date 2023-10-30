91f3271b29880396633231f1d6c43110d67549b6
Joao Victor <134668287+jvjoaovictorguedes@users.noreply.github.com>
2023-10-30 17:33:09 -0300

    upgrade commit

--- script.js
+++ script.js
@@ -1,106 +1,62 @@
-let lista = [];
+const buttonInsert = document.querySelector('#insertDes');
+const entries = [];
 
-function adicionarTarefa(nomeTarefa, prioridade, tempoLevado) {
-    let id = lista.length + 1;
-    lista.push({
-        id: id,
-        nomeTarefa: nomeTarefa,
-        prioridade: prioridade,
-        tempoLevado: tempoLevado
+class DataBase {
+    constructor(amount, expense, price, date){
+        this.amount = amount;
+        this.expense = expense;
+        this.price = price;
+        this.date = date;
+    }
+}
+
+
+buttonInsert.addEventListener("click", function() {
+    const inputDes = document.querySelector('#inputDes').value;
+    const inputPrice = document.querySelector('#priceDes').value;
+    const inputDate = document.querySelector('#dateDes').value;
+    const inputAmount = document.querySelector('#inputAmount').value;
+    
+    const newEntry = new DataBase(inputAmount, inputDes, inputPrice, inputDate);
+    entries.push(newEntry);
+    
+    const outputTable = document.querySelector('#table');
+    
+    outputTable.innerHTML = '';
+
+    const newColumn1 = document.createElement('th');
+    const newColumn2 = document.createElement('th');
+    const newColumn3 = document.createElement('th');
+    const newColumn4 = document.createElement('th');
+
+    newColumn1.textContent = "Quantidade";
+    newColumn2.textContent = "Despesas";
+    newColumn3.textContent = "Preço";
+    newColumn4.textContent = "Data";
+
+    outputTable.appendChild(newColumn1);
+    outputTable.appendChild(newColumn2);
+    outputTable.appendChild(newColumn3);
+    outputTable.appendChild(newColumn4);
+
+
+    entries.forEach(entry => {
+        const newRow = document.createElement('tr');
+        const newCell1 = document.createElement('td');
+        const newCell2 = document.createElement('td');
+        const newCell3 = document.createElement('td');
+        const newCell4 = document.createElement('td');
+        
+        newCell1.textContent = entry.amount;
+        newCell2.textContent = entry.expense;
+        newCell3.textContent = entry.price;
+        newCell4.textContent = entry.date;
+        
+        newRow.appendChild(newCell1);
+        newRow.appendChild(newCell2);
+        newRow.appendChild(newCell3);
+        newRow.appendChild(newCell4);
+        
+        outputTable.appendChild(newRow);
     });
-
-    return id;
-}
-
-function salvarEdicao() {
-    let tarefa = document.getElementById('edicaoDescricao').value;
-    let prior = document.getElementById('edicaoPrioridade').value;
-    let tempo = document.getElementById('edicaoTempoLevado').value;
-
-    adicionarTarefa(tarefa, prior, tempo);
-
-    atualizarListaTarefas();
-}
-
-function atualizarListaTarefas(id) {
-    let listaTarefasElement = document.getElementById('listaTarefas');
-    listaTarefasElement.innerHTML = ' ';
-    console.log(listaTarefasElement)
-    lista.forEach(tarefa => {
-        let listItem = document.createElement('li');
-        listItem.textContent = `Número: ${tarefa.id}, Tarefa: ${tarefa.nomeTarefa}, Tempo Levado: ${tarefa.tempoLevado}, Prioridade: ${tarefa.prioridade}`;
-
-        let editarButton = document.createElement('button');
-        editarButton.textContent = 'Editar';
-        editarButton.id = tarefa.id;
-
-        listItem.appendChild(editarButton);
-        listaTarefasElement.appendChild(listItem);
-
-        editarButton.addEventListener('click', function() {
-            abrirModalEdicao(tarefa.id);
-            
-        });        
-            
-    });
-    // Classificar o elemento com o ID passado
-    if (id) {
-        let elementoClassificado = document.getElementById(id);
-        if (elementoClassificado) {
-            elementoClassificado.classList.add(tarefa.id);
-        }
-    }
-}
-
-
-function editarTarefa(id) {
-    console.log("Função editarTarefa foi chamada.");
-    let tarefaEditada = lista.find(tarefa => tarefa.id == id);
-
-    if (tarefaEditada) {
-        let tarefaAlt = document.getElementById('tarefaAlteracao').value;
-        let prioridadeAlt = document.getElementById('prioridadeAlteracao').value;
-        let tempoAlt = document.getElementById('tempoAlteracao').value;
-
-        tarefaEditada.nomeTarefa = tarefaAlt;
-        tarefaEditada.prioridade = prioridadeAlt;
-        tarefaEditada.tempoLevado = tempoAlt;
-    }
-    atualizarListaTarefas(id);
-}
-
-
-
-function preencherFormularioEdicao(tarefa) {
-    document.getElementById('edicaoDescricao').value = tarefa.nomeTarefa;
-    document.getElementById('edicaoPrioridade').value = tarefa.prioridade;
-    document.getElementById('edicaoTempoLevado').value = tarefa.tempoLevado;
-}
-
-function preencherFomularioAlteracao(tarefa) {
-    document.getElementById('tarefaAlteracao').value = tarefa.nomeTarefa;
-    document.getElementById('prioridadeAlteracao').value = tarefa.prioridade;
-    document.getElementById('tempoAlteracao').value = tarefa.tempoLevado;
-}
-
-function abrirModalEdicao(id) {
-    let form = document.getElementById('modalEdicao');
-    form.style.display = 'block';
-
-    let tarefa = lista.find(item => item.id === id);
-
-    if (tarefa) {
-        preencherFomularioAlteracao(tarefa);
-    }
-}
-
-function fecharModalEdicao(){
-    let form = document.getElementById('modalEdicao');
-    form.style.display = 'none';
-}
-
-
-document.getElementById('formularioEdicao').addEventListener('submit', function(event) {
-    event.preventDefault();
-    salvarEdicao();
 });
